f60c117fee59418fb395d668935eb0c64490074c
Komerdoor <komerdoor@gmail.com>
2021-12-03 20:27:09 +0100

    Using latest Droblox Core

--- src/server/main.server.ts
+++ src/server/main.server.ts
@@ -1,7 +1,7 @@
-import {Instance} from "@rbxts/droblox-core";
+import {findNodes} from "@rbxts/droblox-core/helpers/instance";
 import {Workspace} from "@rbxts/services";
 
 // Print all baseparts in the Lua console
-for (const x of Instance.findNodes(Workspace, (x): x is BasePart => x.IsA("BasePart"))) {
+for (const x of findNodes(Workspace, (x): x is BasePart => x.IsA("BasePart"))) {
   print(x.Name);
 }
