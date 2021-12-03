a55411825fb7425755434191ad449248f104c2a7
Komerdoor <komerdoor@gmail.com>
2021-12-03 05:47:10 +0100

    Print all parts on the screen Fixed Rojo project path

--- src/server/main.server.ts
+++ src/server/main.server.ts
@@ -1,1 +1,7 @@
-export {};
+import {Instance} from "@rbxts/droblox-core";
+import {Workspace} from "@rbxts/services";
+
+// Print all baseparts in the Lua console
+for (const x of Instance.findNodes(Workspace, (x): x is BasePart => x.IsA("BasePart"))) {
+  print(x.Name);
+}
